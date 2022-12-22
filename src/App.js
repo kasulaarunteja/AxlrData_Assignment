5382569d1779f9cba76b10d31321b2865635392e
kasulaarunteja <kasulaarunteja@gmail.com>
2022-12-22 20:37:03 +0530

    login setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Loginpage from './Components/Loginpage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>Product data app</h2>
+     <Loginpage/>
     </div>
   );
 }
---
+++ src/Components/Loginpage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Loginpage = ()=>{
+    return(
+        <div>
+          <h3>Login</h3>
+        </div>
+    )
+}
+
+export default Loginpage
