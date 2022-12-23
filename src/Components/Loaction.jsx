2cf9cf3495908668c7f140eb0ef18595edef03d2
kasulaarunteja <kasulaarunteja@gmail.com>
2022-12-23 20:52:35 +0530

    loaction

---
+++ src/Components/Loaction.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Loaction = () => {
+    return (
+        <div>
+        <Navbar/>
+            <h2>Loaction</h2>
+        </div>
+    );
+}
+
+export default Loaction;
