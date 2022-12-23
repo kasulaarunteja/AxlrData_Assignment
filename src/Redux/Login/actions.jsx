9efb309c70dba3b5553890f986454361a4173851
kasulaarunteja <kasulaarunteja@gmail.com>
2022-12-23 20:53:18 +0530

    login redux

---
+++ src/Redux/Login/actions.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+import { LOG_IN, LOG_OUT } from "./actiontype";
+
+
+// export const logIn = () => {
+//     return (type:LOG_IN)
+// }
+
+// export const logOut = () => {
+//     return (type : LOG_OUT)
+// }
